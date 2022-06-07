bb3221c49a08e892b10dd4d93208b87ecede201e
lmberard <lberard@fi.uba.ar>
2022-06-07 16:46:46 -0300

    libs and deployment for remix ide

--- scripts/deploy_with_web3.ts
+++ scripts/deploy_with_web3.ts
@@ -1,25 +1,14 @@
+// This script can be used to deploy the "Storage" contract using Web3 library.
+// Please make sure to compile "./contracts/1_Storage.sol" file before running this script.
+// And use Right click -> "Run" from context menu of the file to run the script. Shortcut: Ctrl+Shift+S
 
-export const deploy = async (contractName: string, arguments: Array<any>, from?: string, gas?: number) => {
-    
-    console.log(`deploying ${contractName}`)
-    // Note that the script needs the ABI which is generated from the compilation artifact.
-    // Make sure contract is compiled and artifacts are generated
-    const artifactsPath = `browser/contracts/artifacts/${contractName}.json` // Change this for different path
+import { deploy } from './web3-lib'
 
-    const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
-
-    const accounts = await web3.eth.getAccounts()
-
-    let contract = new web3.eth.Contract(metadata.abi)
-
-    contract = contract.deploy({
-        data: metadata.data.bytecode.object,
-        arguments
-    })
-
-    const newContractInstance = await contract.send({
-        from: from || accounts[0],
-        gas: gas || 1500000
-    })
-    return newContractInstance.options    
-}: Promise<any>
+(async () => {
+    try {
+        const result = await deploy('Lottery', [])
+        console.log(`address: ${result.address}`)
+    } catch (e) {
+        console.log(e.message)
+    }
+})()
